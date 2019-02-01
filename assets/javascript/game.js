ce0ce18e54f261fff8e517f252cc3ffa3da3c771
benleahy91 <benleahy91@gmail.com>
2019-01-31 19:44:22 -0500

    cleaned up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,6 @@ var userScore = 0;
 var comScore = 0;
 
 function startGame (event) {
-
 	userScore = 0;
 	comScore = Math.floor(Math.random() * 81) + 19;
 	blueRupee = Math.floor(Math.random() * 12) + 1;
@@ -25,71 +24,61 @@ function startGame (event) {
 }
 
 function checkWin (event) {
-
 	if (userScore === comScore) {
 		alert("You win!");
 		wins++;
 		$("#wins").text(wins);
 		startGame();
-	}
-
-		else if (userScore > comScore) {
+	} else if (userScore > comScore) {
 			alert("You lose!")
 			losses++;
 			$("#losses").text(losses);
 			startGame();
-		}
-}
+	};
+};
 
 startGame();
 	
 $(document).ready(function() {
-	
 	$("#greenrupee").on("click", function() {
 		$(document).ready(function() {
 		userScore = userScore + greenRupee;
 		console.log(userScore);
 		$("#userscore").text(userScore);
-		checkWin()
-
+		checkWin();
 		});
 	});
 });
 
 $(document).ready(function() {
-	
 	$("#bluerupee").on("click", function() {
 		$(document).ready(function() {
 		userScore = userScore + blueRupee;
 		console.log(userScore);
 		$("#userscore").text(userScore);
-		checkWin()
+		checkWin();
 		});
 	});
 });
 
 $(document).ready(function() {
-	
 	$("#redrupee").on("click", function() {
 		$(document).ready(function() {
 		userScore = userScore + redRupee;
 		console.log(userScore);
 		$("#userscore").text(userScore);
-		checkWin()
-
+		checkWin();
 		});
 	});
 });
 
 $(document).ready(function() {
-	
 	$("#yellowrupee").on("click", function() {
 		$(document).ready(function() {
 		userScore = userScore + yellowRupee;
 		console.log(userScore);
 		$("#userscore").text(userScore);
-		checkWin()
-
+		checkWin();
 		});
 	});
 });
